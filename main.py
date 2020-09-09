bd2626c1ea693b7e44e879a2283bdaec12490b71
Long Nguyen <nguyenhailong253@gmail.com>
2020-09-10 09:50:18 +1000

    scrape fruit and veg

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 from src.components.crawler import Crawler
 from src.modules.woolies.woolies import WooliesCrawler
 
 def main():
     crawler = WooliesCrawler()
-    print(crawler.run());
+    crawler.run();
 
 if __name__ == '__main__':
     main()
--- src/modules/woolies/woolies.py
+++ src/modules/woolies/woolies.py
@@ -7,107 +7,107 @@ from .woolies_urls import CATEGORY_URLS
 import json
 import datetime
 import sys
+import csv
+import re
 
-{
-    "url": {
-        "class": "shelfProductTile-imageWrapper",
-        "base": "https://www.woolworths.com.au",
-        "append": "/shop/productdetails/544762/primo-chicken-breast-thinly-sliced"
-    }
-}
+SUPERMARKET = 'Woolworths Supermarket'
+
+CSV_COLUMN_NAMES = [
+    'supermarket',
+    'category',
+    'product_name',
+    'price',
+    'cup_price',
+    'product_url',
+    'img_url',
+    'viewed_date',
+    'ratings',
+    'rating_count',
+    'product_specials',
+    'available_in_stock'
+]
 
 class WooliesCrawler(WooliesBaseCrawler):
 
     def __init__(self):
         super().__init__()
+        self.csv_writer = None
+        self.csv_file = None
+        self.current_category = None
 
-    def scrapping(self, html):
+    def insert_item_to_csv(self, data):
+        self.csv_writer.writerow(data)
 
-        containers = self.get_items_on_page(html)
-        category = self.get_category_title(html)
-        
-        print('Total items in this page: ' + str(len(containers)))
-        print('')
-        
-        arr = []
-        
-        for container in containers:
-            # get the product name
-            product_name = container.find('h3', {'class':'shelfProductTile-description'}).text.strip()
-            # initial product is available
-            availability = True
-            # get the date and time of the scrapping time
-            date_now = datetime.datetime.now()        
+    def process_fields_in_item(self, item):
+        product_name = self.get_product_name(item)
+        viewed_date = self.NOW
+        product_url = self.get_product_url(item)
+        img_url = self.get_product_img_url(item)
+        ratings = self.get_product_ratings(item)
+        rating_count = self.get_rating_count(item)
+        product_specials = self.get_product_specials(item)
+        cup_price = self.get_product_cup_price(item)
+        price = self.get_product_price(item)
+        available_in_stock = True if price else False
 
-            # check price and availability of each item
-            if(container.find('div', {'class': 'shelfProductTile-cupPrice'})):
-                price = container.find('div', {'class': 'shelfProductTile-cupPrice'}).text.strip()
-            elif(container.find('span', {'class':'price-dollars'})):
-                price_dollar = container.find('span',{'class':'price-dollars'})
-                price_cent = container.find('span', {'class': 'price-cents'})
-                price = '$' + price_dollar.text + '.' + price_cent.text
-            else:
-                price = 'Currently not in stock'
-                availability = False
+        return [SUPERMARKET, 
+            self.current_category,
+            product_name,
+            price,
+            cup_price,
+            product_url,
+            img_url,
+            viewed_date,
+            ratings,
+            rating_count,
+            product_specials,
+            available_in_stock]
 
-            obj = {
-                "name": product_name,
-                "price": price,
-                "availability": availability,
-                "datetime": date_now,
-                "category": category,
-                "pic": None
-            }
+    def scrape_items_on_page(self, html):
+        items = self.get_items_on_page(html)
+        print('Total items in this page: {}\n'.format(len(items)))
 
-            #return all the items in the page
-            arr.append(obj)
-        return arr, len(containers)
+        self.current_category = self.get_category_title(html)
+
+        for item in items:
+            data = self.process_fields_in_item(item)
+            
+            if len(data) > 0:
+                self.insert_item_to_csv(data)
+
+        return len(items)
     
-    # convert datetime format to fit json
-    def myconverter(self, o):
-        if isinstance(o, datetime.datetime):
-            return o.__str__()
+    def initialise_csv_writer(self, url):
+        category = url.replace(
+            'https://www.woolworths.com.au/shop/browse/', '').replace(
+                '?pageNumber=', '').replace(
+                    '-', '_')
+        if not self.csv_writer and not self.csv_file:
+            csv_path = './src/modules/woolies/data/{}.csv'.format(category)
+            self.csv_file = open(csv_path, 'w')
+            self.csv_writer = csv.writer(self.csv_file)
+            self.csv_writer.writerow(CSV_COLUMN_NAMES)
+
+    def close_csv(self):
+        if self.csv_file:
+            self.csv_file.close()
+            self.csv_file = None
+            self.csv_writer = None
 
     def run(self):
-        # contain full list details for woolies
-        full_list = []
-        seller = {
-            "seller": {
-                "name": "Woolsworth",
-                "description": "Woolsworth Supermarket",
-                "url": "https://www.woolsworth.com.au",
-                "added_datetime": None
-            }
-        }
-        full_list.append(seller)
-        arr = []  # used to store every object
-
-        # scrapping for each section selected in the list
         for base_url in CATEGORY_URLS:
-            n_items = 1
+            num_items_remaining = 1
             page_number = 1
 
-            while(n_items != 0):
+            self.initialise_csv_writer(base_url)
+
+            while(num_items_remaining != 0):
                 url = self.format_url(base_url, page_number)
                 print('Page ' + str(page_number) + ": " + url)
 
                 html = self.parse_html_page(url)
-
-                arrSinglePage, n_items = self.scrapping(html)
-
-                for obj in arrSinglePage:
-                    arr.append(obj)
+                num_items_remaining = self.scrape_items_on_page(html)
 
                 page_number = page_number + 1
 
-
-        #https://stackoverflow.com/questions/1720421/how-do-i-concatenate-two-lists-in-python
-        # add the products array to the full list
-        products = {'products': arr}
-        full_list.append(products)
-
-        # write a json file on all items
-        with open('wooliesData.json', 'w') as outfile:
-            json.dump(full_list, outfile, default=self.myconverter)
-
-        print(len(arr))
+            self.close_csv()
